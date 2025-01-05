4b93bc37425cf9c840c2468f36d1d39b3dc18b85
Pablo Olle <156226950+impoldev@users.noreply.github.com>
2025-01-05 14:22:53 -0300

    add sections

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,15 @@ import Delivery from '../assets/Delivery.png'
 import GreekSalad from '../assets/GreekSalad.jpg'
 import Bruschetta from '../assets/Bruschetta.svg'
 import LemonDessert from '../assets/LemonDessert.jpg'
+import StarFull from '../assets/StarFull.png'
+import StarHalf from '../assets/StarHalf.png'
+import StarEmpty from '../assets/StarEmpty.png'
+import T1 from '../assets/T1.jpg'
+import T2 from '../assets/T2.jpg'
+import T3 from '../assets/T3.jpg'
+import T4 from '../assets/T4.jpg'
+import MarioAndAdrianA from '../assets/Mario-and-Adrian-A.jpg'
+import MarioAndAdrianB from '../assets/Mario-and-Adrian-B.jpg'
 
 export function Main() {
   return(
@@ -64,6 +73,81 @@ export function Main() {
         </div>
       </section>
 
+      <section className='testimonials-container'>
+        <h2>Testimonials</h2>
+        <div className='testimonials'>
+          <div>
+            <div className='rating'>
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarEmpty} alt="empty star" width={10} />
+            </div>
+            <div className='person'>
+              <img src={T1} alt="Clare Honey" width={70} />
+              <h3>Clare Honey</h3>
+            </div>
+            <q className='testimonial'>Amazing food, great ambiance, and top-notch service. Highly recommend for restaurant lovers like me!</q>
+          </div>
+          <div>
+            <div className='rating'>
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+            </div>
+            <div className='person'>
+              <img src={T2} alt="John Smith" width={70} />
+              <h3>John Smith</h3>
+            </div>
+            <q className='testimonial'>Delicious food and impeccable service. The atmosphere was warm and inviting. I loved it</q>
+          </div>
+          <div>
+            <div className='rating'>
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarHalf} alt="half start" width={10} />
+            </div>
+            <div className='person'>
+              <img src={T3} alt="Julia Moon" width={70} />
+              <h3>Julia Moon</h3>
+            </div>
+            <q className='testimonial'>An absolute treat! The menu had so many unique options, and each bite was better than the last.</q>
+          </div>
+          <div>
+            <div className='rating'>
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+              <img src={StarFull} alt="full star" width={10} />
+            </div>
+            <div className='person'>
+              <img src={T4} alt="Susan Spill" width={70} />
+              <h3>Susan Spill</h3>
+            </div>
+            <q className='testimonial'>Fantastic meal and a great food experience. I will surely come back soon for more. Five stars!</q>
+          </div>
+        </div>
+      </section>
+
+      <section className='about'>
+        <div className='text'>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>At Little Lemon, we believe that food is more than just nourishment—it’s an art form. 
+            Our talented chefs bring passion and creativity to every plate, combining fresh, seasonal ingredients with global influences. 
+            Whether you’re enjoying a cozy dinner with loved ones or celebrating a special occasion, we aim to make every moment memorable.</p>
+        </div>
+        <div className='images'>
+          <img className='one' src={MarioAndAdrianB} alt="Mario and Adrian B"  />
+          <img className='two' src={MarioAndAdrianA} alt="Mario and Adrian A"  />
+        </div>
+      </section>
 
     </main>
   )
