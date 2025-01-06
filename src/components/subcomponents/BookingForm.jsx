1e07c7e3251e08a30c5dcdf6eccbb6ed64926920
Pablo Olle <156226950+impoldev@users.noreply.github.com>
2025-01-06 12:52:21 -0300

    update form

--- src/components/subcomponents/BookingForm.jsx
+++ src/components/subcomponents/BookingForm.jsx
@@ -47,19 +47,36 @@ export function BookingForm() {
   <form onSubmit={handleSubmit} style={{display: 'grid', maxWidth: '200px', gap: '10px'}}>
     <label htmlFor="res-date">Choose date</label>
     <input onChange={(e) => setDate(e.target.value)} value={date} type="date" id="res-date" />
-    <label htmlFor="res-time">Choose time</label>
-    <select onChange={(e) => setTime(e.target.value)} value={time} id="res-time">
-      {
-        availableTimes.map(t => <option key={t}>{t}</option>)
-      }
-    </select>
-    <label htmlFor="guests">Number of guests</label>
-    <input onChange={(e) => setGuests(e.target.value)} value={guests} type="number" placeholder="1" min="1" max="10" id="guests" />
-    <label htmlFor="occasion">Occasion</label>
-    <select onChange={(e) => setOccasion(e.target.value)} value={occasion} id="occasion">
-        <option>Birthday</option>
-        <option>Anniversary</option>
-    </select>
+    {
+      !(date === '' || (date < new Date().toISOString() && new Date().getDate() !== date.slice(date.length - 2) - 0)) &&
+      <>
+        <table style={{backgroundColor:'white', borderRadius:'16px'}}>
+          <thead>
+            <tr>
+              <th>Available times</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            availableTimes.map(t => <tr key={t}><td style={{borderTop: '1px solid #edefee', padding: '0'}}>{t}</td></tr>)
+            }
+          </tbody>
+        </table>
+        <label htmlFor="res-time">Choose time</label>
+        <select onChange={(e) => setTime(e.target.value)} value={time} id="res-time">
+          {
+            availableTimes.map(t => <option key={t}>{t}</option>)
+          }
+        </select>
+        <label htmlFor="guests">Number of guests</label>
+        <input onChange={(e) => setGuests(e.target.value)} value={guests} type="number" placeholder="1" min="1" max="10" id="guests" />
+        <label htmlFor="occasion">Occasion</label>
+        <select onChange={(e) => setOccasion(e.target.value)} value={occasion} id="occasion">
+            <option>Birthday</option>
+            <option>Anniversary</option>
+        </select>
+      </>
+    }
     <input className='form-button' disabled={date === '' || (date < new Date().toISOString() && new Date().getDate() !== date.slice(date.length - 2) - 0)} type="submit" value="Make your reservation" />
     {
       success &&
