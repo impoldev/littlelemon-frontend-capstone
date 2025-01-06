a498a6c3401601e6fef6fd2778d43dc4f07493c4
Pablo Olle <156226950+impoldev@users.noreply.github.com>
2025-01-05 21:27:44 -0300

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BookingPage } from './components/BookingPage.jsx';
+import { FormContext } from "./App.js";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('TextEncoder is globally defined in Jest', () => {
+  expect(global.TextEncoder).toBeDefined();
 });
+
+test('TextDecoder decodes byte arrays', () => {
+  const bytes = new Uint8Array([84, 101, 115, 116]);
+  const decoder = new TextDecoder('utf-8');
+  const result = decoder.decode(bytes);
+  expect(result).toBe('Test');
+});
+
+test('Renders the BookingForm heading', () => {
+
+    render(
+      <FormContext.Provider value={{date: '2025-01-17', setDate: () => {}, time: '17:00', setTime: () => {}, guests: '1', setGuests: () => {}, occasion: 'Birthday', setOccasion: () => {}, availableTimes: ['17:00', '18:00'], dispathAvailableTimes: () => {}}}>
+        <BookingPage />
+      </FormContext.Provider>
+    );
+    const headingElement = screen.getByText("Book Now");
+    expect(headingElement).toBeInTheDocument();
+})
--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -5,7 +5,7 @@ export function BookingPage() {
   return(
     <main>
       <section className="heading">
-        <h1>Booking Online</h1>
+        <h1>Book Now</h1>
       </section>
       <section className="body">
         <p>Submit this form to book your table. We hope to see you soon!</p>
